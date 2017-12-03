8bf7a26eb6e825919e43c53f88fc81cdff9c1ca9
Vitalii Tartachnyi <vitartach@gmail.com>
2017-12-03 16:55:38 +0200

    Add python files

---
+++ src/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import numpy as np
+from math import *
+
+class AuxiliaryMath:
+    #sigmoid function
+    @staticmethod
+    def sigmoid (x):
+        g = np.vectorize (lambda x: 1/(1+exp(-x)))
+        return g(x)
+
+class NeuralNet:
+    def __init__ (self, inputQuantity, nodesOnLayers): #inputQuantity - int, nodesOnLayers - [int, int, .., int]
+        self.nNodes = 0
+        self.nWeights = 0
+        self.nLayers = 0
+        self.inp = None # input vector (input nodes)
+        self.n = []  # list by layers of nodes vectors
+        self.b = []  # list by layers of biases vectors
+        self.w = [] # list by layers of weights matrices
+        self.layersIndex = []
+        if isinstance(nodesOnLayers, list) and isinstance(inputQuantity, int):
+            if all(isinstance(ins, int) for ins in nodesOnLayers):
+                self.nNodes = inputQuantity
+                self.nLayers = len(nodesOnLayers)
+                self.layersIndex = range(self.nLayers)
+                self.inp = np.zeros (inputQuantity)
+                quantityOnPrevious = None
+                for i in self.layersIndex:
+                    if i==0:
+                        quantityOnPrevious = inputQuantity
+                    else:
+                        quantityOnPrevious = nodesOnLayers[i-1]
+                    self.nNodes += nodesOnLayers[i]
+                    self.nWeights = self.nWeights + nodesOnLayers[i]*quantityOnPrevious
+                    self.n.append (np.zeros (nodesOnLayers[i], dtype=np.float)) #initialization of nodes on each layers
+                    self.b.append (np.random.rand(nodesOnLayers[i])) #initialization of biases on each layers
+                    self.w.append(np.random.rand(nodesOnLayers[i], quantityOnPrevious)) #initialization of weights on each layers
+            else:
+                print ("Quantity of nodes must be integer")
+        else:
+            print ("Please enter neural network construction in following format (numberNodesOnInput, [numberNodesOn1Layer, numberNodesOn2Layer, ... , numberNodesOnNLayer])")
+
+    def input (self, inputParameters):
+        if (len(inputParameters) == self.inp.size):
+            self.inp = np.array (inputParameters)
+        else:
+            print ("Please put correct quantity of input values")
+
+    def update (self):
+        for i in self.layersIndex:
+            if i==0:
+                self.n[i] = AuxiliaryMath.sigmoid(self.w[i].dot(self.inp)+self.b[i])
+            else:
+                self.n[i] = AuxiliaryMath.sigmoid(self.w[i].dot(self.n[i-1])+self.b[i])
+        return None
