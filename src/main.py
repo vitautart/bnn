869bf71ba422317274d13c8092d2342e387eb6f6
Vitalii Tartachnyi <vitartach@gmail.com>
2017-12-09 19:42:21 +0200

    Add backpropagation and some controls

--- src/main.py
+++ src/main.py
@@ -13,20 +13,15 @@ class AuxiliaryMath:
     #first derivative of vector sigmoid function
     @staticmethod
     def dsigmoid (x):
-        sigm = lambda x: 1/(1+exp(-x))
+        sigma = lambda x: 1/(1+exp(-x))
         g = np.vectorize (lambda x: sigma(x)*(1-sigma(x)))
         return g(x)
+
+
 
 class InputReader:
     def __init__ (self):
         return 0
-
-class Teacher:
-    def __init__ (self):
-        return 0
-
-    def backpropagation (self):
-        return 0;
 
 class NeuralNet:
     def __init__ (self, inputQuantity, nodesOnLayers): #inputQuantity - int, nodesOnLayers - [int, int, .., int]
@@ -37,6 +32,11 @@ class NeuralNet:
         self.n = []  # list by layers of nodes vectors
         self.b = []  # list by layers of biases vectors
         self.w = [] # list by layers of weights matrices
+        self.z = []  # list by layers of auxiliary vectors of arguments of sigmoid
+        #backpropagator vectors
+        self.delta = []
+        self.dcdw = []
+        self.dcdb = []
         self.layersIndex = []
         if isinstance(nodesOnLayers, list) and isinstance(inputQuantity, int):
             if all(isinstance(ins, int) for ins in nodesOnLayers):
@@ -53,12 +53,31 @@ class NeuralNet:
                     self.nNodes += nodesOnLayers[i]
                     self.nWeights = self.nWeights + nodesOnLayers[i]*quantityOnPrevious
                     self.n.append (np.zeros (nodesOnLayers[i], dtype=np.float)) #initialization of nodes on each layers
+                    self.z.append (np.zeros (nodesOnLayers[i], dtype=np.float))#initialization of arguments of sigmoid on each layers
                     self.b.append (np.random.rand(nodesOnLayers[i])) #initialization of biases on each layers
                     self.w.append(np.random.rand(nodesOnLayers[i], quantityOnPrevious)) #initialization of weights on each layers
             else:
                 print ("Quantity of nodes must be integer")
         else:
             print ("Please enter neural network construction in following format (numberNodesOnInput, [numberNodesOn1Layer, numberNodesOn2Layer, ... , numberNodesOnNLayer])")
+
+    def backpropagate (self, desOutp): #desOutp - desirable output vector
+        y = np.array(desOutp)
+        i = self.nLayers-1
+        while i>=0:
+            print(i)
+            if i == (self.nLayers-1):
+                self.delta.append((self.output()-y) * AuxiliaryMath.dsigmoid (self.z[i]))
+            else:
+                self.delta.insert(0, np.transpose(self.w[i+1]).dot(self.delta[0]) * AuxiliaryMath.dsigmoid(self.z[i]))
+            i=i-1
+        for i in self.layersIndex:
+            if i == 0:
+                self.dcdw.append (np.outer(self.delta[0], self.inp))
+            else:
+                self.dcdw.append (np.outer(self.delta[i], self.n[i-1]))
+
+
 
     def input (self, inputParameters):
         if (len(inputParameters) == self.inp.size):
@@ -69,10 +88,24 @@ class NeuralNet:
     def update (self):
         for i in self.layersIndex:
             if i==0:
-                self.n[i] = AuxiliaryMath.sigmoid(self.w[i].dot(self.inp)+self.b[i])
+                self.z[i] = self.w[i].dot(self.inp)+self.b[i]
+                self.n[i] = AuxiliaryMath.sigmoid(self.z[i])
             else:
+                self.z[i] = self.w[i].dot(self.n[i-1])+self.b[i]
                 self.n[i] = AuxiliaryMath.sigmoid(self.w[i].dot(self.n[i-1])+self.b[i])
 
+
+    def push (self, eta):
+        for i in self.layersIndex:
+            self.w[i] = self.w[i]-eta*self.dcdw[i]
+            self.b[i] = self.b[i]-eta*self.delta[i]
+
     def output (self):
-        print (self.n[self.nLayers-1])
+        #print (self.n[self.nLayers-1])
         return self.n[self.nLayers-1]
+
+    def verify (self,inputParameters, desOutp):
+        self.input(inputParameters)
+        self.update ()
+        g = np.vectorize (lambda x: pow(x, 2))
+        return g(self.output()-desOutp)
--- src/test.py
+++ src/test.py
@@ -5,17 +5,31 @@ from math import *
 
 
 
-nn = main.NeuralNet (8, [4, 4, 2])
+nn = main.NeuralNet (8, [4, 3, 3])
 nn.input([0.6, 0.4, 0.6, 0.2, 0.2, 0.8, 0.3, 0.9])
 nn.update()
-
 print ("This is test!")
 print ("number of layers = " + str(nn.nLayers))
 print ("number of nodes = " + str(nn.nNodes))
 print ("number of weights = " + str(nn.nWeights))
+
+print (nn.output())
+
+nn.backpropagate ([0.0, 1.0, 0.0])
+#print (nn.delta)
+#print (nn.dcdw)
+nn.push(20)
+
+print (nn.verify([0.6, 0.4, 0.6, 0.2, 0.2, 0.8, 0.3, 0.9], [0.0, 1.0, 0.0]))
+print (nn.output())
+
+
+
 nn.output()
 
 #print (nn.inp)
 #print (nn.n)
 #print (nn.b)
 #print (nn.w)
+#print (nn.delta)
+#print (nn.dcdw)
